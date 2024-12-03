250ef5dc5dcbd4172deb1829b96002fc6b63a90d
Sathvik Birudavolu <8377116+BSathvik@users.noreply.github.com>
2024-12-02 22:25:28 -0600

    solve p2

--- bin/p1_part2.ml
+++ bin/p1_part2.ml
@@ -1,36 +1,45 @@
-let file = "p1.txt";
-module IntMap = Map.Make(Int)
+let file = "p1.txt"
+
+module IntMap = Map.Make (Int)
 
 let () =
-  let lines = 
+  let lines =
     let file_in = open_in file in
-    In_channel.input_lines file_in;
+    In_channel.input_lines file_in
   in
 
-  let split = List.map (fun line ->
-   String.split_on_char ' ' line
-    |> List.filter ( fun s -> s <> "" )
-    |> List.map int_of_string
-  ) lines in
+  let split =
+    List.map
+      (fun line ->
+        String.split_on_char ' ' line
+        |> List.filter (fun s -> s <> "")
+        |> List.map int_of_string)
+      lines
+  in
 
   let sorted_column i =
-    List.map (fun pair -> List.nth pair i) split
-    |> List.sort Int.compare in 
+    List.map (fun pair -> List.nth pair i) split |> List.sort Int.compare
+  in
 
   let col_a = sorted_column 0 in
   let col_b = sorted_column 1 in
 
-  let freq_b = List.fold_left (fun acc id -> 
-    IntMap.update id ( function 
-      | Some count -> Some (count + 1)
-      | None -> Some 1
-    ) acc 
-  ) IntMap.empty col_b in
+  let freq_b =
+    List.fold_left
+      (fun acc id ->
+        IntMap.update id
+          (function Some count -> Some (count + 1) | None -> Some 1)
+          acc)
+      IntMap.empty col_b
+  in
 
-  let total = List.fold_left (fun acc id ->
-    match IntMap.find_opt id freq_b with
-      | Some count -> acc + (id * count)
-      | None -> acc
-  ) 0 col_a in
+  let total =
+    List.fold_left
+      (fun acc id ->
+        match IntMap.find_opt id freq_b with
+        | Some count -> acc + (id * count)
+        | None -> acc)
+      0 col_a
+  in
 
   print_int total
---
+++ bin/p2.ml
@@ -0,0 +1,31 @@
+let file = "data/p2.txt"
+
+open List
+
+let is_safe report =
+  let rec diffs = function
+    | [] | _ :: [] -> []
+    | x :: y :: t -> (y - x) :: diffs (y :: t)
+  in
+
+  for_all (fun n -> n > 0 && n <= 3) (diffs report)
+  || for_all (fun n -> n < 0 && n >= -3) (diffs report)
+
+let part1 reports = filter is_safe reports |> length
+
+let part2 reports =
+  let remove i l = filteri (fun j _ -> i <> j) l in
+  let report_combos r = init (length r) (fun i -> remove i r) in
+
+  filter (fun report -> exists is_safe (report_combos report)) reports |> length
+
+let () =
+  let reports =
+    map
+      (fun line -> String.split_on_char ' ' line |> map int_of_string)
+      (open_in file |> In_channel.input_lines)
+  in
+
+  print_int (part1 reports);
+  print_newline ();
+  print_int (part2 reports)
