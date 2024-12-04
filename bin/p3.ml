23383717583ab95a792e5637a6cc05d5bd8b4910
Sathvik Birudavolu <8377116+BSathvik@users.noreply.github.com>
2024-12-04 14:10:35 -0600

    clean up p3

--- bin/p3.ml
+++ bin/p3.ml
@@ -1,58 +1,39 @@
 let file = "data/p3.txt"
 
 open List
+open Str
 
-let part1 memory =
-  let mul_r = Str.regexp {|mul(\([0-9][0-9]?[0-9]?\),\([0-9][0-9]?[0-9]?\))|} in
+let mul_r = regexp {|mul(\([0-9][0-9]?[0-9]?\),\([0-9][0-9]?[0-9]?\))|}
 
-  let rec total start =
+let match_with_pos apply r str =
+  let rec aux start =
     try
-      let start = Str.search_forward mul_r memory start in
-      let mul =
-        (int_of_string @@ Str.matched_group 1 memory)
-        * (int_of_string @@ Str.matched_group 2 memory)
-      in
-      mul + total (1 + start)
-    with _ -> 0
+      let start_pos = search_forward r str start in
+      let value = apply str in
+      (start_pos, value) :: aux (1 + start_pos)
+    with _ -> []
   in
-
-  total 0
+  aux 0
 
 type op = Mul of int * int | Do | Dont
 
+let mult_with_pos str =
+  let parse_group i s = int_of_string (matched_group i s) in
+  match_with_pos (fun s -> Mul (parse_group 1 s, parse_group 2 s)) mul_r str
+
+let rec total enabled = function
+  | [] -> 0
+  | Do :: t -> total true t
+  | Dont :: t -> total false t
+  | Mul (a, b) :: t ->
+      if enabled then (a * b) + total enabled t else total enabled t
+
+let part1 memory = mult_with_pos memory |> map (fun (_, op) -> op) |> total true
+
 let part2 memory =
-  let mul_r = Str.regexp {|mul(\([0-9][0-9]?[0-9]?\),\([0-9][0-9]?[0-9]?\))|} in
-
-  let rec mult_with_pos start =
-    try
-      let start = Str.search_forward mul_r memory start in
-      let mul =
-        Mul
-          ( int_of_string @@ Str.matched_group 1 memory,
-            int_of_string @@ Str.matched_group 2 memory )
-      in
-      (start, mul) :: mult_with_pos (1 + start)
-    with _ -> []
-  in
-
-  let rec switch_pos start r =
-    try
-      let start = Str.search_forward r memory start in
-      start :: switch_pos (1 + start) r
-    with _ -> []
-  in
-
-  let rec total enabled = function
-    | [] -> 0
-    | Do :: t -> total true t
-    | Dont :: t -> total false t
-    | Mul (a, b) :: t ->
-        if enabled then (a * b) + total enabled t else total enabled t
-  in
-
-  mult_with_pos 0
-  @ map (fun pos -> (pos, Do)) (switch_pos 0 (Str.regexp {|do()|}))
-  @ map (fun pos -> (pos, Dont)) (switch_pos 0 (Str.regexp {|don't()|}))
+  mult_with_pos memory
+  @ match_with_pos (fun _ -> Do) (regexp {|do()|}) memory
+  @ match_with_pos (fun _ -> Dont) (regexp {|don't()|}) memory
   |> sort (fun (a, _) (b, _) -> Int.compare a b)
   |> map (fun (_, op) -> op)
   |> total true
