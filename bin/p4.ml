f73b9697001fab72cb57b508f652a49234fa34c3
Sathvik Birudavolu <sathvikb30@gmail.com>
2024-12-06 23:59:56 -0500

    p4 part 1 works

--- bin/p4.ml
+++ bin/p4.ml
@@ -3,44 +3,27 @@ let file = "data/p4.txt"
 open List
 
 let part1 lines =
-  (* map (fun line -> String.concat "," (map str line)) lines |> iter print_endline *)
   let char (i, j) = nth (nth lines i) j in
-  (* let range n = Seq.init n (fun i -> i) in *)
-  (* let rev n = Seq.init n (fun i -> n - i) in *)
   let row, col = (length lines, length (nth lines 0)) in
 
   let horizontal = Seq.init row (fun i -> Seq.init col (fun j -> (i, j))) in
   let vertical = Seq.init col (fun j -> Seq.init row (fun i -> (i, j))) in
 
-  (* let up = *)
-  (*   Seq.map *)
-  (*     (fun (s_i, s_j) -> *)
-  (*       Seq.init (min (row - s_i) (col - s_j)) (fun k -> (s_i + k, s_j + k))) *)
-  (*     (Seq.append *)
-  (*        (Seq.init (row - 1) (fun i -> (i + 1, 0))) *)
-  (*        (Seq.init col (fun j -> (0, j)))) *)
-  (* in *)
-  (* let down = *)
-  (*   Seq.map *)
-  (*     (fun (s_i, s_j) -> *)
-  (*       Seq.init (min (row - s_i) (col - s_j)) (fun k -> (s_i + k, s_j - k))) *)
-  (*     (Seq.append *)
-  (*        (Seq.init (row - 1) (fun i -> (i + 1, 0))) *)
-  (*        (Seq.init col (fun j -> (row - 1, j)))) *)
-  (* in *)
   let down =
     Seq.append
       (Seq.init row (fun i ->
            Seq.init (min (row - i) col) (fun k -> (i + k, k))))
       (Seq.init (col - 1) (fun j ->
-           Seq.init (min row (col - j + 1)) (fun k -> (k, j + k + 1))))
+           Seq.init (min row (col - j - 1)) (fun k -> (k, j + k + 1))))
   in
 
-  (* Seq.iter (Seq.iter (fun (i, j) -> Format.printf "%d:%d," i j)) up_diagonal; *)
-  (* print_endline "------"; *)
-  Seq.iter (Seq.iter (fun (i, j) -> Format.printf "%d:%d\n" i j)) down;
+  let up =
+    Seq.append
+      (Seq.init row (fun i -> Seq.init (min (i + 1) col) (fun k -> (i - k, k))))
+      (Seq.init (col - 1) (fun j ->
+           Seq.init (min row (col - j - 1)) (fun k -> (row - k - 1, j + k + 1))))
+  in
 
-  (* print_endline "------"; *)
   let rec count seq =
     match String.of_seq (Seq.map char (Seq.take 4 seq)) with
     | "" -> 0
@@ -52,7 +35,7 @@ let part1 lines =
     (Seq.fold_left
        (fun acc seq -> acc + count seq)
        0
-       ([ horizontal; vertical; down ] |> to_seq |> Seq.concat))
+       ([ horizontal; vertical; down; up ] |> to_seq |> Seq.concat))
 
 let () =
   let lines =
