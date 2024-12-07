aaada0876e41dc17ca46289d07ae5cd720fa1930
Sathvik Birudavolu <sathvikb30@gmail.com>
2024-12-07 00:51:54 -0500

    day 4 part 2 works

--- bin/p4.ml
+++ bin/p4.ml
@@ -37,10 +37,35 @@ let part1 lines =
        0
        ([ horizontal; vertical; down; up ] |> to_seq |> Seq.concat))
 
+let part2 lines =
+  let char (i, j) = nth (nth lines i) j in
+  let row, col = (length lines, length (nth lines 0)) in
+
+  let count i j =
+    let xmas =
+      map
+        (fun (r, c) -> (i + r, j + c))
+        [ (0, 0); (1, 1); (2, 2); (0, 2); (1, 1); (2, 0) ]
+    in
+
+    match xmas |> map char |> to_seq |> String.of_seq with
+    | "" -> 0
+    | "MASMAS" | "SAMSAM" | "SAMMAS" | "MASSAM" -> 1
+    | _ -> 0
+  in
+
+  let horizontal =
+    Seq.concat
+      (Seq.init (row - 2) (fun i -> Seq.init (col - 2) (fun j -> (i, j))))
+  in
+  print_int (Seq.fold_left (fun acc (i, j) -> acc + count i j) 0 horizontal)
+
 let () =
   let lines =
     open_in file |> In_channel.input_lines
     |> map (fun line -> String.to_seq line |> of_seq)
   in
 
-  part1 lines
+  part1 lines;
+  print_endline "----";
+  part2 lines
